9f1b9d6ec303bf94d59557871d24e83a87521b2b
Aadil-Shabir <aadil.shabir13@gmail.com>
2022-06-26 14:15:16 +0500

    feat/print-functionality

--- components/Notifications.js
+++ components/Notifications.js
@@ -1,6 +1,6 @@
-import { useState, useContext, useRef } from 'react';
-import { GlobalContext } from '../context/GlobalContext';
-import ViewReport from '../components/ViewReport';
+import { useState, useContext, useRef } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+import ViewReport from "./ViewReport";
 import {
   Flex,
   Text,
@@ -24,7 +24,7 @@ import {
   CloseButton,
   useToast,
   Circle,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {
   SunIcon,
   MoonIcon,
@@ -37,34 +37,44 @@ import {
   CopyIcon,
   CloseIcon,
   CheckIcon,
-} from '@chakra-ui/icons';
-import { BsPrinterFill } from 'react-icons/bs';
-import { useRouter } from 'next/router';
+} from "@chakra-ui/icons";
+import { BsPrinterFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 
-export default function Notifications() {
+import ReactToPrint from "react-to-print";
+
+const Notifications = () => {
   const { data, selected, setSelected, filteredReports } =
     useContext(GlobalContext);
-  const modalBg = useColorModeValue('gray.50', 'gray.900');
-  const sectionBg = useColorModeValue('gray.50', 'gray.700');
+  const modalBg = useColorModeValue("gray.50", "gray.900");
+  const sectionBg = useColorModeValue("gray.50", "gray.700");
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [scrollBehavior, setScrollBehavior] = useState('inside');
+  const [scrollBehavior, setScrollBehavior] = useState("inside");
   const btnRef = useRef(null);
   const toast = useToast();
   const router = useRouter();
+  const componentRef = useRef();
+
+  const printHandler = () => {
+    // alert(
+    //   "window.print(), where you can send to printer and save as PDF, Perhaps react-to-print package, where you can select graphics background. You cannot print in dark"
+    // );
+    return <button>Print</button>;
+  };
 
   return (
-    <Flex w='full' flexDir='column'>
+    <Flex w="full" flexDir="column">
       {data.map((item, index) => (
         <Flex
           key={index}
-          h='36px'
-          _hover={{ bg: 'gray.300' }}
-          w='full'
+          h="36px"
+          _hover={{ bg: "gray.300" }}
+          w="full"
           my={1}
           p={3}
-          align='center'
+          align="center"
           onDoubleClick={() => setSelected(item._id)}
-          cursor='pointer'
+          cursor="pointer"
         >
           {item.wellName}
         </Flex>
@@ -76,101 +86,122 @@ export default function Notifications() {
             finalFocusRef={btnRef}
             isOpen={selected}
             scrollBehavior={scrollBehavior}
-            size='2xl'
+            size="2xl"
             key={index}
           >
             <ModalOverlay />
             <ModalContent bg={modalBg}>
               <ModalHeader p={3}>
-                <Flex w='full' justify='end'>
+                <Flex w="full" justify="end">
                   <Flex>
-                    <CloseButton size='sm' onClick={() => setSelected(null)} />
+                    <CloseButton size="sm" onClick={() => setSelected(null)} />
                   </Flex>
                 </Flex>
               </ModalHeader>
 
-              <div className='overflow-x-auto overflow-y-auto scrollbar-hide'>
-                <ViewReport
-                  _id={item._id}
-                  wellType={item.wellType}
-                  wellName={item.wellName}
-                  dateSubmitted={item.dateSubmitted}
-                  submittedBy={item.submittedBy}
-                  company={item.company}
-                  field={item.field}
-                  fluid={item.fluid}
-                  environment={item.environment}
-                  status={item.status}
-                  completion={item.completion}
-                  wellProfile={item.wellProfile}
-                  spudDate={item.spudDate}
-                  originalElevation={item.originalElevation}
-                  units={item.units}
-                  latitude={item.latitude}
-                  longitude={item.longitude}
-                  wellRemarks={item.wellRemarks}
-                  jobDescription={item.jobDescription}
-                  jobStartDate={item.jobStartDate}
-                  jobStartTime={item.jobStartTime}
-                  jobEndDate={item.jobEndDate}
-                  jobEndTime={item.jobEndTime}
-                  conveyance={item.conveyance}
-                  operation={item.operation}
-                  runs={item.runs}
-                  jobRemarks={item.jobRemarks}
-                  personnel={item.personnel}
-                  equipment={item.equipment}
-                  sequenceOfEvents={item.sequenceOfEvents}
-                  pce={item.pce}
-                  pceRunNumber={item.pceRunNumber}
-                  nptHours={item.nptHours}
-                  pceMinId={item.pceMinId}
-                  pceTotalLength={item.pceTotalLength}
-                  toolstring={item.toolstring}
-                  toolstringRunNumber={item.toolstringRunNumber}
-                  toolstringTotalLength={item.toolstringTotalLength}
-                  toolstringTotalWeight={item.toolstringTotalWeight}
-                  toolstringMaxOd={item.toolstringMaxOd}
-                  attachments={item.attachments}
-                />
+              <div className="overflow-x-auto overflow-y-auto scrollbar-hide">
+                <div>
+                  {/* <ReactToPrint
+                    trigger={() => <button>Print</button>}
+                    content={() => componentRef.current}
+                  /> */}
+                  <ViewReport
+                    _id={item._id}
+                    ref={componentRef}
+                    wellType={item.wellType}
+                    wellName={item.wellName}
+                    dateSubmitted={item.dateSubmitted}
+                    submittedBy={item.submittedBy}
+                    company={item.company}
+                    field={item.field}
+                    fluid={item.fluid}
+                    environment={item.environment}
+                    status={item.status}
+                    completion={item.completion}
+                    wellProfile={item.wellProfile}
+                    spudDate={item.spudDate}
+                    originalElevation={item.originalElevation}
+                    units={item.units}
+                    latitude={item.latitude}
+                    longitude={item.longitude}
+                    wellRemarks={item.wellRemarks}
+                    jobDescription={item.jobDescription}
+                    jobStartDate={item.jobStartDate}
+                    jobStartTime={item.jobStartTime}
+                    jobEndDate={item.jobEndDate}
+                    jobEndTime={item.jobEndTime}
+                    conveyance={item.conveyance}
+                    operation={item.operation}
+                    runs={item.runs}
+                    jobRemarks={item.jobRemarks}
+                    personnel={item.personnel}
+                    equipment={item.equipment}
+                    sequenceOfEvents={item.sequenceOfEvents}
+                    pce={item.pce}
+                    pceRunNumber={item.pceRunNumber}
+                    nptHours={item.nptHours}
+                    pceMinId={item.pceMinId}
+                    pceTotalLength={item.pceTotalLength}
+                    toolstring={item.toolstring}
+                    toolstringRunNumber={item.toolstringRunNumber}
+                    toolstringTotalLength={item.toolstringTotalLength}
+                    toolstringTotalWeight={item.toolstringTotalWeight}
+                    toolstringMaxOd={item.toolstringMaxOd}
+                    attachments={item.attachments}
+                  />
+                </div>
               </div>
 
               <ModalFooter p={3} mt={6}>
-                <Flex w='full' justify='space-between'>
+                <Flex w="full" justify="space-between">
                   <Flex>
                     <Button
-                      onClick={() => alert('open edit form')}
-                      size='sm'
+                      onClick={() => alert("open edit form")}
+                      size="sm"
                       leftIcon={<EditIcon />}
-                      variant='outline'
-                      colorScheme='blue'
-                      w='80px'
+                      variant="outline"
+                      colorScheme="blue"
+                      w="80px"
                       mr={3}
                     >
                       Edit
                     </Button>
 
-                    <Button
-                      onClick={() =>
-                        alert(
-                          'window.print(), where you can send to printer and save as PDF, Perhaps react-to-print package, where you can select graphics background. You cannot print in dark'
-                        )
-                      }
-                      size='sm'
+                    {/* This is a new button */}
+
+                    <ReactToPrint
+                      trigger={() => (
+                        <Button
+                          size="sm"
+                          leftIcon={<BsPrinterFill />}
+                          variant="outline"
+                          colorScheme="blue"
+                        >
+                          Print
+                        </Button>
+                      )}
+                      content={() => componentRef.current}
+                    />
+
+                    {/* This is the old button  */}
+
+                    {/* <Button
+                      onClick={printHandler}
+                      size="sm"
                       leftIcon={<BsPrinterFill />}
-                      variant='outline'
-                      colorScheme='blue'
+                      variant="outline"
+                      colorScheme="blue"
                     >
                       Print
-                    </Button>
+                    </Button> */}
                   </Flex>
                   <Flex>
                     <Button
-                      size='sm'
-                      variant='outline'
-                      colorScheme='green'
+                      size="sm"
+                      variant="outline"
+                      colorScheme="green"
                       leftIcon={<CheckIcon />}
-                      onClick={() => alert('set as Verified')}
+                      onClick={() => alert("set as Verified")}
                     >
                       Approve
                     </Button>
@@ -182,4 +213,6 @@ export default function Notifications() {
         ))}
     </Flex>
   );
-}
+};
+
+export default Notifications;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,39 @@
-import { Flex, Button, Text, useColorMode, IconButton } from '@chakra-ui/react';
-import { SunIcon, MoonIcon } from '@chakra-ui/icons';
-import Notifications from '../components/Notifications';
+import { useRef } from "react";
+
+import { Flex, Button, Text, useColorMode, IconButton } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import Notifications from "../components/Notifications";
+
+import ReactToPrint from "react-to-print";
 
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const componentRef = useRef();
   return (
-    <Flex w='100vw' h='100vh' justify='space-between'>
-      <Flex w='220px' borderRightWidth='1px'>
+    <Flex w="100vw" h="100vh" justify="space-between">
+      <Flex w="220px" borderRightWidth="1px">
         nav
       </Flex>
-      <Flex w='678px' flexDir='column' p={3}>
-        <Flex w='full' justify='end'>
-          {colorMode === 'light' ? (
+      <Flex w="678px" flexDir="column" p={3}>
+        <Flex w="full" justify="end">
+          {colorMode === "light" ? (
             <IconButton
               icon={<MoonIcon />}
-              size='sm'
+              size="sm"
               onClick={toggleColorMode}
             />
           ) : (
             <IconButton
               icon={<SunIcon />}
-              size='sm'
+              size="sm"
               onClick={toggleColorMode}
             />
           )}
         </Flex>
-        <Notifications />
+
+        <Notifications jref={componentRef} />
       </Flex>
-      <Flex w='220px' borderLeftWidth='1px'>
+      <Flex w="220px" borderLeftWidth="1px">
         aside
       </Flex>
     </Flex>
